863a6723f6435fc89a5051e9b65f31a90687cb8c
NoorAtallah <nooratallah1234@gmail.com>
2024-08-13 16:58:44 +0300

    navbar header contactus uploaded

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BillboardDesignPage from "./pages/Advertisement";
+import { Nav } from "./components/header";
+import { Foot } from './components/footer';
+import ContactForm from "./pages/contact";
 
 function App() {
   return (
-    <div className="App">
-      <BillboardDesignPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/Advertisement" element={<BillboardDesignPage />} />
+          <Route path="/contact" element={<ContactForm />} />
+
+          
+        </Routes>
+        <Foot />
+      </div>
+    </Router>
   );
 }
 
