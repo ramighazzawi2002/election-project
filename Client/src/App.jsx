9e56cee59e6b6d6bb7f51c6b35c9bd5e5cb2f171
Islamomer47 <islamomar0003@gmail.com>
2024-08-13 15:25:43 +0300

    ads

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,12 @@
+import React from "react";
+import BillboardDesignPage from "./pages/Advertisement";
+
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <BillboardDesignPage />
+    </div>
+  );
 }
 
 export default App;
