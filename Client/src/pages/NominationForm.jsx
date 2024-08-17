f3fb1c98c060710ad2723dda6336e552703d6c6a
ramighazzawi2002 <rami.ghazzawiabed@gmail.com>
2024-08-17 18:09:33 +0300

    redirect to login if not login

--- Client/src/pages/NominationForm.jsx
+++ Client/src/pages/NominationForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const CustomAlert = ({ title, description }) => (
   <div
@@ -14,6 +15,7 @@ const CustomAlert = ({ title, description }) => (
 const NominationForm = () => {
   const [candidates, setCandidates] = useState([]);
   const [usersData, setUsersData] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
     (async () => {
       try {
